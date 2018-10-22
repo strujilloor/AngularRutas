da471a7646d0bc93d71bbb96c3b7457275b0f2a5
strujilloor <strujillo1293@gmail.com>
2018-10-22 12:59:51 -0500

    added components and service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { TaskComponent } from './components/task/task.component';
+import { TaskListComponent } from './components/task-list/task-list.component';
+import { TaskFormComponent } from './components/task-form/task-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    TaskComponent,
+    TaskListComponent,
+    TaskFormComponent
   ],
   imports: [
     BrowserModule
