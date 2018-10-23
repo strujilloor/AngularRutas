5bcd271d9590a7963b13947572323b441ea43b13
strujilloor <strujillo1293@gmail.com>
2018-10-23 00:19:23 -0500

    agregado bootstrap al proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,10 +14,7 @@ const appRoutes: Routes = [
     { path: 'task-form', component: TaskFormComponent },
     { path: 'task-list', component: TaskListComponent },
     { path: 'task/:title/:description', component: TaskComponent },
-    { path: '',
-        redirectTo: '/task-form',
-        pathMatch: 'full'
-    }
+    { path: '', redirectTo: '/task-form', pathMatch: 'full'}
 ];
 
 
